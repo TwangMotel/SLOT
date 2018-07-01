7cab966e600467d706a6576a5236e00aa38e6ef5
pmaxwellward <pmaxwellward@gmail.com>
2018-07-01 12:20:28 -0500

    updated Node2 Robbery example

--- SLOT_proto/Assets/NodeMap/Scripts/Movement/ClickMove.cs
+++ SLOT_proto/Assets/NodeMap/Scripts/Movement/ClickMove.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 using JSNodeMap;
 using System.Collections.Generic;
+using Yarn.Unity;
 
 [RequireComponent(typeof(Agent))]
 public class ClickMove : MonoBehaviour {
 	private Agent agent;
 	public bool active;
 	public float maxDistance = 1000f;
+    GameObject dialogue;
+    DialogueRunner dialogueRunner;
 
-    
 
-	void Awake () {
+    void Awake () {
         agent = GetComponent<Agent>();
+        dialogue = GameObject.Find("Dialogue");
+        dialogueRunner = dialogue.GetComponent<DialogueRunner>();
+        
        
 	}
 	
 	void Update () {
-		if (Input.GetMouseButtonDown(0) && ! agent.isMoving && active) {
+		if (Input.GetMouseButtonDown(0) && ! agent.isMoving && active && dialogueRunner.isDialogueRunning == false) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
 			if (Physics.Raycast(ray, out hit, maxDistance)) {
 				Node hitNode = hit.transform.GetComponent<Node>();
-                List<Node> neighbors = Pathfinding.GetNeighbors(hitNode);
+                List<Node> neighbors = Pathfinding.GetNeighbors(hitNode,agent);
 				if (hitNode != null && neighbors.Contains(agent.currentNode)) {
 					agent.MoveToTarget(hitNode);
 				}
