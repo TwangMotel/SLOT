a587cc376756a1d71f9e1fc279e57bfcee8a0963
CalderTwang <rcalderking@gmail.com>
2018-07-05 22:11:42 -0500

    camera follow

---
+++ SLOT_proto/Assets/Scripts/camerafollow.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+                //for Smoothdamp use below
+    //private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+                    //for smoothdamp use below
+        //Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        
+    }
+
+
+}
